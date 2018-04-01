6eaf1ff34d50ea79a2340fa19c64fbcc7256673c
matjack9 <matjack9@gmail.com>
2018-03-31 20:44:05 -0400

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,10 @@
-export default function manageUsers(state, action){
+const init = { users: [] };
+
+export default function manageUsers(state = init, action) {
+	switch (action.type) {
+		case "ADD_USER":
+			return { ...state, users: [...state.users, action.user] };
+		default:
+			return state;
+	}
 }
